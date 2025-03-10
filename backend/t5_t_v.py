35021d4de8610e5e566e9ce64ff58bc21b2925e8
Andrey QRQM <andrey.madyarov@gmail.com>
2025-03-10 16:46:52 +0300

    backup

--- backend/t5_t_v.py
+++ backend/t5_t_v.py
@@ -1,9 +1,28 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_dataset, concatenate_datasets
+import os
 
+"""DESCRIBE THE MODEL"""
 model_name = "google/flan-t5-small"  # Change this if needed
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
+"""INTRODUCE TRAINING"""
+json_files = ["AI_dungeon_master_tool/backend/dnd-5e-srd/json/01 races.json",
+              "dnd-5e-srd/json/00 legal.json",
+              "AI_dungeon_master_tool/backend/dnd-5e-srd/json/02 classes.json"]
+
+datasets = []
+
+for json_file in json_files:
+    dataset = load_dataset("json", data_files=json_file)
+    datasets.append(dataset["train"])  # Assuming 'train' is the key you want
+
+combined_dataset = concatenate_datasets(datasets)
+
+print(combined_dataset[10:])
+
+"""GENERATE OUTPUT"""
 def generate_text(prompt):
     inputs = tokenizer(prompt, return_tensors="pt")
     outputs = model.generate(**inputs, max_length=100)
